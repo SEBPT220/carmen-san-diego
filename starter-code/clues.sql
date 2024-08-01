cd1bb461d3cd12e4c4cc9d00d20991d96dc7aa4b
rSchleckser <rickyschleckser@gmail.com>
2024-08-01 01:15:02 -0700

    feat: complete Carmen Sandiego Deliverable

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -2,47 +2,81 @@
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
+SELECT * FROM country 
+WHERE region = 'Southern Europe' 
+ORDER BY population ASC LIMIT 1;
 
-
+VAT  | Holy See (Vatican City State) | Europe    | Southern Europe |         0.4 |      1929 |       1000 |                | 9.00 |        | Santa Sede/Citt∩┐╜ del Vaticano | Independent Church State | Johannes Paavali II |    3538
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
+SELECT language FROM countrylanguage 
+WHERE countrycode = 'VAT';
 
-
+ language
+----------
+ Italian
+(1 row)
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
+SELECT country.*, countrylanguage.* 
+FROM countrylanguage 
+JOIN country ON countrylanguage.countrycode = country.code 
+WHERE country.region = 'Southern Europe' 
+AND countrylanguage.language = 'Italian' 
+AND countrylanguage.percentage = 100;
 
-
+    name
+------------
+ San Marino
+(1 row)
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
 -- There are only two cities she could be flying to in the country. One is named the same as the country – that
 -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
 --  be flying to.
+SELECT * FROM city WHERE countrycode = 'SMR' AND name != 'San Marino';
 
+  id  |    name    | countrycode |     district      | population
+------+------------+-------------+-------------------+------------
+ 3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+SELECT country.*, city.* 
+FROM city 
+JOIN country 
+ON city.countrycode = country.code 
+WHERE country.continent ='South America' 
+AND city.name LIKE 'Serra%';
 
 
-
+ BRA  | Brazil | South America | South America | 8.547403e+06 |      1822 |  170115000 |           62.9 | 776739.00 | 804108.00 | Brasil    | Federal Republic | Fernando Henrique Cardoso |     211 | BR    | 265 | Serra | BRA         | Esp∩┐╜rito Santo |     302666
+(1 row)
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
+SELECT city.name AS capital_city 
+FROM country JOIN city 
+ON city.id = country.capital 
+WHERE country.code = 'BRA';
 
+ capital_city
+--------------
+ Bras∩┐╜lia
+(1 row)
 
 -- Clue #7:  She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
 -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
 -- landing dock. 
 -- Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
 -- if we can crack it, we can finally put her where she belongs – behind bars.
-
-
 
 
 -- Our playdate of late has been unusually fun –
@@ -52,6 +86,10 @@
 -- So I'm off to add one to the population I find
 -- In a city of ninety-one thousand and now, eighty five.
 
+SELECT name FROM city WHERE population = 91084;
 
+     name
+--------------
+ Santa Monica
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
