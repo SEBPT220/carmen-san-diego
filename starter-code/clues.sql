693a7b4fa105c0d7a104fe351af2449cbfc62314
tflinch <tflinch16@gmail.com>
2024-08-03 14:10:03 -0400

    feat: answer to clue #7

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -52,4 +52,4 @@ SELECT city.name AS capital_city FROM country JOIN city ON city.id = country.cap
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
-
+SELECT name FROM city WHERE population = 91084;
