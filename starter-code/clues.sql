deb60cc0990953d7c1bc16e18787c980b24ba104
Miagotobene <miagotobene@gmail.com>
2024-08-04 16:05:27 -0400

    solution to clue 3

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -5,7 +5,7 @@
 -- Find value of min population. Solution: Found that the least populated country in Southern Europe has a population of 1000
 SELECT MIN(population) FROM country WHERE region = 'Southern Europe' ; 
 
--- Found that the country with 1000 population is Holy See: Vatican City with code: VAT 
+-- Found that the country with 1000 population is Vatican City with code: VAT 
 SELECT * FROM country WHERE continent = 'Europe' AND region = 'Southern Europe' AND population = 1000;
 
 
@@ -20,6 +20,16 @@ SELECT * FROM countrylanguage WHERE countrycode = 'VAT' AND isofficial = 't' ;
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
+SELECT MAX(percentage) FROM countrylanguage WHERE language = 'Italian' ; 
+-- Found that the max percentage is 100
+SELECT * FROM countrylanguage WHERE language = 'Italian' AND isofficial = 't' AND percentage = 100;
+-- Country code with max language Italian and max percentage 100 is SMR
+
+SELECT * FROM country WHERE code = 'SMR'; 
+-- The solution to CLUE 3: SAN Marino
+
+
+
 
 
 
