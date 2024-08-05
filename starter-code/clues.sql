ccf989c58f53345ec1f2eb574280a9d4c2d1e61d
Miagotobene <miagotobene@gmail.com>
2024-08-04 22:23:40 -0400

    feat: solution to clue 6 and 7

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -48,6 +48,7 @@ SELECT country.*, city.* FROM city JOIN country ON city.countrycode = country.co
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
+SELECT city.name AS capital_city FROM country JOIN city ON city.id = country.capital WHERE country.code = 'BRA';
 
 
 -- Clue #7:  She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
@@ -68,4 +69,4 @@ SELECT country.*, city.* FROM city JOIN country ON city.countrycode = country.co
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
-
+SELECT name FROM city WHERE population = 91084;
