36f2cf60030bdb8e8dc945a7c1aabd25cecd94f1
Farid Virani <fvirani786@gmail.com>
2024-08-10 15:29:13 -0500

    Feat:deliverables

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -2,19 +2,32 @@
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
-
+SELECT name
+FROM country
+WHERE region = 'Southern Europe'
+ORDER BY population ASC
+LIMIT 1;
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
+SELECT language
+FROM countrylanguage
+WHERE countrycode = (SELECT code FROM country WHERE name = 'San Marino')
+AND isofficial = 'T';
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
 
+SELECT c2.name
+FROM country c1
+JOIN countrylanguage cl ON c1.code = cl.countrycode
+JOIN country c2 ON cl.language = (SELECT language FROM countrylanguage WHERE countrycode = c1.code AND isofficial = 'T')
+WHERE c1.name = 'San Marino' AND cl.isofficial = 'T' AND c2.name <> c1.name;
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
@@ -22,17 +35,32 @@
 -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
 --  be flying to.
 
+SELECT name
+FROM city
+WHERE countrycode = (SELECT code FROM country WHERE name = 'Italy') 
+AND name <> 'Italy';
+
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+SELECT name, country.name
+FROM city
+JOIN country ON city.countrycode = country.code
+WHERE city.name LIKE 'Mil%' 
+AND city.name <> 'Milan';
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
+
+SELECT name
+FROM city
+WHERE id = (SELECT capital FROM country WHERE name = 'Ecuador');
+
 
 
 
@@ -41,6 +69,10 @@
 -- landing dock. 
 -- Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
 -- if we can crack it, we can finally put her where she belongs – behind bars.
+
+SELECT destination
+FROM flights
+WHERE departure = 'Quito';
 
 
 
